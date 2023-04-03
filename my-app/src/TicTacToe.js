0968432d5add22b1acc4a8ca148c943f77e8cfae
xkostolanskym <kostolansky.marek@gmail.com>
2023-04-03 22:02:10 +0200

    check for winner

--- my-app/src/TicTacToe.js
+++ my-app/src/TicTacToe.js
@@ -5,6 +5,41 @@ const TicTacToe = () => {
   const [turn, setTurn] = useState("x");
   const [cells, setCells] = useState(Array(9).fill(""));
 
+  const checkForWinner = (squares) => {
+    let combos = {
+      across: [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+      ],
+      down: [
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+      ],
+      diagonal: [
+        [0, 4, 8],
+        [2, 4, 6],
+      ],
+    };
+
+    for (let combo in combos) {
+      combos[combo].forEach((pattern) => {
+        if (
+          squares[pattern[0]] === "" ||
+          squares[pattern[1]] === "" ||
+          squares[pattern[2]] === ""
+        ) {
+          // do nothing
+        } else if (
+          squares[pattern[0]] === squares[pattern[1]] &&
+          squares[pattern[1]] === squares[pattern[2]]
+        ) {
+        }
+      });
+    }
+  };
+
   const handleClick = (num) => {
     if (cells[num] !== "") {
       alert("aleardy clicked");
@@ -19,9 +54,9 @@ const TicTacToe = () => {
       squares[num] = "o";
       setTurn("x");
     }
-    setCells(squares);
 
-    console.log(squares);
+    checkForWinner(squares);
+    setCells(squares);
   };
 
   const Cell = ({ num }) => {
