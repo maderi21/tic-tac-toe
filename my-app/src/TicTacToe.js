4068de8dfb2dc5a40c9b439a5bd572571b42ec60
xkostolanskym <kostolansky.marek@gmail.com>
2023-04-03 21:15:38 +0200

    change player logic

--- my-app/src/TicTacToe.js
+++ my-app/src/TicTacToe.js
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import "./TicTacToe.css";
 
 const TicTacToe = () => {
+  const [turn, setTurn] = useState("x");
+
+  const handleClick = (num) => {
+    if (turn === "x") {
+      setTurn("o");
+    } else {
+      setTurn("x");
+    }
+  };
+
+  const Cell = ({ num }) => {
+    return <td onClick={() => handleClick(num)}>-</td>;
+  };
+
   return (
-    <div>
-      <h1>Tic tac Toe</h1>
+    <div className="container">
+      <table>
+        Turn:{turn}
+        <tbody>
+          <tr>
+            <Cell num={0} />
+            <Cell num={1} />
+            <Cell num={2} />
+          </tr>
+          <tr>
+            <Cell num={3} />
+            <Cell num={4} />
+            <Cell num={5} />
+          </tr>
+          <tr>
+            <Cell num={6} />
+            <Cell num={7} />
+            <Cell num={8} />
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 };
